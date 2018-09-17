37299d2772d6ed5163e1c3c2f04c7340de441815
tei0513 <254207025@qq.com>
2018-09-17 15:25:23 +0800

    test

---
+++ src/main/java/com/tss/test/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.tss.test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("test")
+public class TestController {
+	
+	@RequestMapping
+	@ResponseBody
+	public String initView() {
+		return "hello world";
+	}
+}
