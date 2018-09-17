d28c48551b86f4e76103271eae90964e4548967a
tei0513 <32868272+tei0513@users.noreply.github.com>
2018-09-17 15:30:57 +0800

    Update App.java

--- src/main/java/com/tss/App.java
+++ src/main/java/com/tss/App.java
@@ -11,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class App {
 	
+	// 启动函数
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 	}
