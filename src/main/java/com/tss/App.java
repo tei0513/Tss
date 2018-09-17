197ca93ef5d80da13a65417193a7c2840908319d
tei0513 <254207025@qq.com>
2018-09-17 15:29:55 +0800

    修改注释

--- src/main/java/com/tss/App.java
+++ src/main/java/com/tss/App.java
@@ -2,7 +2,12 @@ package com.tss;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * springboot启动类。
+ * 
+ * @author Administrator
+ * @Date 2018年9月17日
+ */
 @SpringBootApplication
 public class App {
 	
